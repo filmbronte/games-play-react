335be274a68ffe7dfa79f5c9346910c104bcf2d5
filmbronte <122818201+filmbronte@users.noreply.github.com>
2023-09-20 20:36:28 +0300

    break app into components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import {Header} from "./components/Header/Header";
+import {Home} from "./components/Home/Home";
+import {CreatePage} from "./components/CreatePage/CreatePage";
+import {EditPage} from "./components/EditPage/EditPage";
+import {DetailsPage} from "./components/DetailsPage/DetailsPage";
+import {Catalogue} from "./components/Catalogue/Catalogue";
+
 function App() {
-  return (
-    <div className="App">
-    games play
-    </div>
-  );
+    return (
+        <div id="box">
+            <Header/>
+            <Home/>
+            <CreatePage/>
+            <EditPage/>
+            <DetailsPage/>
+            <Catalogue/>
+        </div>
+    );
 }
 
 export default App;
